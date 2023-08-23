3307b54c5c57a201c7b7664e5477a5b85b68a7cb
agenious1122 <ageenious@gmail.com>
2023-08-23 18:02:35 +0500

    updated

---
+++ Q6.js
@@ -0,0 +1,8 @@
+"use strict";
+// Q No.6.	Write a program that takes temperature and check it. 
+// If it is cold then suggest the user to wear warm 
+// clothes and so on according to the weather.
+var tempr = 21;
+if (tempr < 20) {
+    console.log("You should wear warm clothes");
+}
